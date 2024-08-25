468f90cae94f76c44682199da43728d5dcf8fca2
19983467897 <2715158815.com>
2024-08-25 21:39:54 +0800

    feat: update latest

--- blog-v3/src/components/ChatRoom/index.vue
+++ blog-v3/src/components/ChatRoom/index.vue
@@ -110,8 +110,7 @@ const initWebsocket = async (isReconnect = false) => {
     websocket = null;
   }
 
-  websocket = new WebSocket("ws://mrzym.top/ws/");
-  // websocket = new WebSocket("ws://localhost:8889/");
+  websocket = new WebSocket("ws://localhost:8889/");
 
   if (websocket) {
     websocket.onopen = () => {
